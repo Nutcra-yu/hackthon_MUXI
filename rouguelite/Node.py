94377b46dccf13c1f063c6717982fae29ebe8915
jackj-ohn <2105753640@qq.com>
2022-05-21 11:45:53 +0800

    first

---
+++ rouguelite/Node.py
@@ -0,0 +1,10 @@
+import pygame
+
+# 主要是随机生成Node并且将他们连接起来
+class Node:
+    def __init__(self):
+        # 有不同的图片
+        self.image = pygame.image.load().convert()
+        self.object = ""
+    def desc(self,desc):
+        self.description = desc
---
+++ rouguelite/drawmap.py
@@ -0,0 +1,6 @@
+import pygame
+import Node
+
+start = Node.Node.desc("起点")
+
+end = Node.Node.desc("终点")
