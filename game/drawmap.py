108e05deef9bbc5f6d053b91c233b66e7ef3fba5
jackj-ohn <2105753640@qq.com>
2022-05-22 06:17:00 +0800

    datails

--- game/drawmap.py
+++ game/drawmap.py
@@ -1,16 +1,14 @@
 import sys
-
 import pygame
 import Node
 import func
 
 
-def drawmain(screen):
-    attr = (90, 90)
-    screen.fill((196, 168, 124))
+def drawmain(screen,setting):
+    screen.fill(setting.bg_color)
 
     start = Node.Node("../village.png", "起点", (20, 20), "village")
-    end = Node.Node("../village.png", "终点", (878, 50), "village")
+    end = Node.Node("../village.png", "终点", (setting.screen_width - setting.attr[0] - 50,setting.screen_height), "village")
 
     group = pygame.sprite.Group()
 
@@ -18,17 +16,17 @@ def drawmain(screen):
 
     node = start
 
-    func.drawbackground(screen)
+    func.drawbackground(screen,setting)
 
     path = []
     flag = True
     while True:
         if flag:
-            flag = func.drawMenue(screen, group, attr, start, end)
+            flag = func.drawMenue(screen, group, setting.attr, start, end,setting)
 
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
-                node = node.judgePos(screen, event, group, attr, node)
+                node = node.judgePos(screen, event, group, setting.attr, node)
                 path.append(node)
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -39,7 +37,7 @@ def drawmain(screen):
             #         screen.fill((196, 168, 124))
             #         pygame.display.flip()
             #         time.sleep(1)
-            #         func.reload(path, group, screen, start)
+            #         func.reload(path, group, screen, start,setting)
             #         pygame.display.flip()
 
         group.draw(screen)
