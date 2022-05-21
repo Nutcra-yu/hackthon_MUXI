2a58c465a5721dadf91f2e075d8db46a051276de
flsdqm <592661681@qq.com>
2022-05-21 12:18:01 +0800

    settings

---
+++ game/settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    def __init__(self):
+        # 屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        # 人的设置
+        self.health = 100
